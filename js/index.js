7d4d595dc07c31d0ed5e5e1b8e325d64dc19ae9c
Berqia Mouad <83679093+BerqiaMouad@users.noreply.github.com>
2022-04-30 22:29:13 +0000

    third commit

---
+++ js/index.js
@@ -0,0 +1,54 @@
+function transite() {
+    const transition_ele = document.querySelector('.transition-1');
+    transition_ele.classList.add('active_transition');
+    var typed_transite = new Typed(".texttransite", {
+        strings: ["^1000Let's Go!", "^500About The Club."],
+        typedSpeed: 30,
+        backSpeed: 30,
+        loop: false
+    })
+    setTimeout(() => {
+        transition_ele.classList.remove('active_transition');
+    }, 5000);
+}
+
+function transite_2() {
+    const transition_ele = document.querySelector('.transition-2');
+    transition_ele.classList.add('active_transition');
+    var typed_transite = new Typed(".texttransite2", {
+        strings: ["^1000Check It Out", "^500Our Activities."],
+        typedSpeed: 30,
+        backSpeed: 30,
+        loop: false
+    })
+    setTimeout(() => {
+        transition_ele.classList.remove('active_transition');
+    }, 5000);
+}
+
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+
+function go_to(tartoadd, tartoremove){
+    var e1 = document.getElementById(tartoremove);
+    var e2 = document.getElementById(tartoadd);
+    transite();
+    setTimeout(() => {
+        e1.classList.remove('active');
+        e2.classList.add('active');
+    }, 5000);
+}
+
+function go_to_2(tartoadd, tartoremove){
+    var e1 = document.getElementById(tartoremove);
+    var e2 = document.getElementById(tartoadd);
+    transite_2();
+    setTimeout(() => {
+        e1.classList.remove('active');
+        e2.classList.add('active');
+    }, 5000);
+}
+
+
---
+++ js/typing.js
@@ -0,0 +1,19 @@
+var typed1 = new Typed(".typing1", {
+    strings: ["^800Welcome!", "To FSR CP Club", "Your road to expert!"],
+    typedSpeed: 30,
+    backSpeed: 60,
+    loop: false
+})
+
+// var typed_about = new Typed(".about_text", {
+//     strings: ["^1000About Us!"],
+//     typedSpeed: 30,
+//     backSpeed: 60,
+//     loop: false
+// })
+// var typed_act = new Typed(".act_text", {
+//     strings: ["^1000Activities"],
+//     typedSpeed: 30,
+//     backSpeed: 60,
+//     loop: false
+// })
