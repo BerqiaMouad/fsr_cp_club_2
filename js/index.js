2a559b1d0f6639114f80850d28f63c817fe8117b
Berqia Mouad <83679093+BerqiaMouad@users.noreply.github.com>
2022-05-01 20:33:59 +0000

    update

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// ======================================================================
 function transite() {
     const transition_ele = document.querySelector('.transition-1');
     transition_ele.classList.add('active_transition');
@@ -26,6 +27,20 @@ function transite_2() {
     }, 5000);
 }
 
+function transite_3() {
+    const transition_ele = document.querySelector('.transition-3');
+    transition_ele.classList.add('active_transition');
+    var typed_transite = new Typed(".texttransite3", {
+        strings: ["^1000Alright!", "You are enjoying this then!", "Then here is a detailed description."],
+        typedSpeed: 30,
+        backSpeed: 30,
+        loop: false
+    })
+    setTimeout(() => {
+        transition_ele.classList.remove('active_transition');
+    }, 6000);
+}
+// ======================================================================
 
 function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
@@ -51,4 +66,12 @@ function go_to_2(tartoadd, tartoremove){
     }, 5000);
 }
 
-
+function go_to_3(tartoadd, tartoremove){
+    var e1 = document.getElementById(tartoremove);
+    var e2 = document.getElementById(tartoadd);
+    transite_3();
+    setTimeout(() => {
+        e1.classList.remove('active');
+        e2.classList.add('active');
+    }, 6000);
+}
